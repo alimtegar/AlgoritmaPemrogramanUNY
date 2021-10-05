82678ff4a222aeb884cd10ed73ca7752d63834bc
Alim Tegar <42492971+alimtegar@users.noreply.github.com>
2021-10-05 12:12:46 +0700

    Update kasir.py

--- kasir.py
+++ kasir.py
@@ -5,7 +5,7 @@ totalSpending = int(input())
 print()
 if (totalSpending > 100000):
     print('Diskon: 5%')
-    print('Total akhir belanja: ' + str(int(totalSpending * 9.5)))
+    print('Total akhir belanja: ' + str(int(totalSpending * 0.95)))
 else:
     print('Diskon: 0%')
     print('Total akhir belanja: ' + str(totalSpending))
